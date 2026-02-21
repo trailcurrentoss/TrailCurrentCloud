8b5e1c7b7451c23f53254462f64c1f4fedf30c24
davidrfloydii <davidrfloydii@gmail.com>
2026-02-21 16:51:13 -0600

    Fixing mount for mosquito mount to certs

--- mosquitto/entrypoint.sh
+++ mosquitto/entrypoint.sh
@@ -11,4 +11,13 @@ rm -f /mosquitto/config/passwd
 mosquitto_passwd -b -c /mosquitto/config/passwd "$MQTT_USERNAME" "$MQTT_PASSWORD"
 chown mosquitto:mosquitto /mosquitto/config/passwd
 
+# Copy TLS certs from read-only host mount and fix ownership so the
+# mosquitto user can read them (host files may be owned by root/other UIDs)
+mkdir -p /mosquitto/certs
+cp /mosquitto/host-certs/server.crt /mosquitto/certs/server.crt
+cp /mosquitto/host-certs/server.key /mosquitto/certs/server.key
+cp /mosquitto/host-certs/ca.crt     /mosquitto/certs/ca.crt
+chown mosquitto:mosquitto /mosquitto/certs/*
+chmod 600 /mosquitto/certs/server.key
+
 exec mosquitto "$@"
